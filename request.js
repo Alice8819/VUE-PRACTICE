0f4f0ec2aac1ba083f2b07dd921852393a6654be
Alice8819 <2802554118@qq.com>
2020-06-26 20:53:49 +0800

    23

--- request.js
+++
@@ -1,7 +0,0 @@
-axios.defaults.baseURL = "https://developer.duyiedu.com/vue/bz";
-
-axios.interceptors.response.use(response => {
-    if(response.status === 200){
-        return response.data
-    }
-})
