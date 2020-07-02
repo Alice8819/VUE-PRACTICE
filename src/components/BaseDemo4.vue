8e1f36b90a76cbe4850e91c8799322a918530f55
Alice8819 <2802554118@qq.com>
2020-07-02 13:25:48 +0800

    28

--- src/components/BaseDemo4.vue
+++ src/components/BaseDemo4.vue
@@ -1,31 +1,47 @@
 <template>
   <div class="demo">
-    <button @click="handleClick">点击</button>
-    <transition mode="in-out">
-      <div :key="keyName" class="box">hello {{keyName}}</div>
-    </transition>
+    <button @click="addLists">添加</button>
+    <button @click="removeLists">移除</button>
+    <button @click="resortLists">洗牌</button>
+      <transition-group tag="ul">
+        <!-- <ul> -->
+        <li v-for="num in lists" :key="num">{{num}}</li>
+        <!-- </ul> -->
+      </transition-group>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      keyName: "Alice"
+      show: true,
+      lists: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      number: 11
     };
   },
-  methods: {
-    handleClick() {
-      const isAlice = this.keyName === "Alice";
-      this.keyName = isAlice ? "Wang" : "Alice";
+    methods: {
+      addLists() {
+        const ranIndex = Math.random() * this.lists.length;  // 随机索引
+        this.lists.splice(ranIndex, 0, this.number);  // 数组添加
+        this.number++;
+      },
+      removeLists() {
+        const ranIndex = Math.random() * this.lists.length; // 随机索引
+        this.lists.splice(ranIndex, 1);   // 数组删除
+      },
+      resortLists() {
+        this.lists.sort(() => {    // 洗牌
+          return Math.random() - 0.5;
+        });
+      }
     }
-  }
 };
 </script>
 
 <style scoped>
 .v-enter {
   opacity: 0;
-  transform: translateX(80px);
+  transform: translateY(30px);
 }
 .v-enter-active,
 .v-leave-active {
@@ -33,33 +49,27 @@ export default {
 }
 .v-enter-to {
   opacity: 1;
-  transform: translateX(0);
+  transform: translateY(0);
 }
 
 .v-leave {
   opacity: 1;
-  transform: translateX(0);
+  transform: translateY(0);
 }
 .v-leave-to {
   opacity: 0;
-  transform: translateX(-80px);
+  transform: translateY(30px);
 }
-.demo {
-  margin-left: 100px;
-}
-.box {
-  width: 150px;
-  height: 30px;
-  background: gray;
-  color: #fff;
-  line-height: 30px;
-  text-align: center;
-  margin-top: 10px;
-  margin-left: 20px;
+li {
   display: inline-block;
-  position: absolute;
+  margin: 0 5px;
 }
-button {
-  display: block;
+
+.v-move {
+  transition:transform .3s
+}
+ul{
+  padding:0;
+  /* margin:0; */
 }
 </style>
